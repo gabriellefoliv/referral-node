08f258f31e87778356545338c1483cef60b1229f
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2025-02-22 12:25:44 -0300

    feat: rota /subscriptions com docs no swagger

---
+++ src/routes/subscripte-to-event-route.ts
@@ -0,0 +1,31 @@
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
+import z from "zod"
+
+export const subscriptToEventRoute: FastifyPluginAsyncZod = async (app) => {
+    app.post('/subscriptions', {
+        schema: {
+            summary: 'Subscribes someone to the event',
+            tags: ['subscripttions'],
+            description: 'asdaaadsass',
+            body: z.object({
+                name: z.string(),
+                email: z.string().email()
+            }),
+            response: {
+                201: z.object({
+                    name: z.string(),
+                    email: z.string()
+                })
+            }
+        }
+    }, async (request, reply) => {
+        const { name, email } = request.body
+    
+        // criação da inscrição no banco de dados
+    
+        return reply.status(201).send({
+            name,
+            email,
+        })
+    })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,43 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from "@fastify/cors"
+import {
+    validatorCompiler,
+    serializerCompiler,
+    ZodTypeProvider,
+    jsonSchemaTransform,
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import { subscriptToEventRoute } from './routes/subscripte-to-event-route'
 
-const app = fastify()
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, {
+    origin: true, // também permite qualquer frontend, porém dá pra limitar com string com a url do frontend
+})
+
+// deixar qualquer frontend acessar a aplicação
+// app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: 'Referrals API',
+            version: '0.0.1',
+        }
+    },
+    transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
+})
+
+app.register(subscriptToEventRoute)
 
 app.get('/hello', () => {
     return 'Hello World'
