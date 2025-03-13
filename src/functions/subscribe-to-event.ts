88cae891709540fa87cb48513c11e809e1f438fa
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2025-03-13 19:07:40 -0300

    feat: get-subscriber-invite-clicks route

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -9,6 +9,17 @@ export async function subscribeToEvent({
   name,
   email,
 }: SubscribeToEventParams) {
+  const subscribers = await prisma.subscription.findMany({
+    where: {
+      email,
+    },
+  })
+
+  if (subscribers.length > 0) {
+    // Ao invés de passar um erro de email já cadastrado, ele reaproveita o email já usado
+    return { subscriberId: subscribers[0].id }
+  }
+
   const subscriber = await prisma.subscription.create({
     data: {
       name,
